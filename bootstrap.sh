c15a18dfafe8dbdcfda002cab447a1ede0e30227
Bayu Setiawan <bayu.tuodeschawk@gmail.com>
2019-07-02 11:54:39 +0700

    install php

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
-apt-get update
+#
+# source https://www.vultr.com/docs/how-to-install-apache-24-mariadb-10-and-php-7x-on-ubuntu-16-04
+apt-get update -y
 # install apache
 apt-get install -y apache2
 if ! [ -L /var/www ]; then
@@ -12,7 +13,7 @@ sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/a
 systemctl restart apache2.service
 
 # mariadb (search mirror in mariadb website)
-apt-get install software-properties-common
+apt-get install software-properties-common -y
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://sgp1.mirrors.digitalocean.com/mariadb/repo/10.4/ubuntu xenial main'
 
@@ -28,3 +29,9 @@ systemctl enable mariadb.service
 /vagrant_data/tools/mysql_secure.sh "password"
 
 # create database
+
+# install php 7.1
+apt-get install -y python-software-properties
+add-apt-repository -y ppa:ondrej/php
+apt-get update -y
+apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip
